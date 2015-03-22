b97c5700be9c316886cff7fffc8111fbb232dde7
Xu ZHANG <laonawuli@126.com>
2015-03-22 11:26:23 -0700

    Code.

---
+++ SearchEngineServer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SearchEngineServer
+//
+//  Created by Xu ZHANG on 3/22/15.
+//  Copyright (c) 2015 Xu ZHANG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
